5dbbbfd345ae3c05dde1d0543839e3562f1198b2
Francesco <G00425679@atu.ie>
2023-03-31 20:08:04 +0100

    Analysis of Iris Data Set

--- analysis.py
+++ analysis.py
@@ -1,24 +1,34 @@
 
 import pandas as pd
+import numpy as np
+
 
 # read in the dataset from the UCI Machine Learning Repository link
 csv_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-#adding the varible attribute_names as variable names in the UCI repository are lcated in a separate file
+# Define the names of the columns in the dataset
 attribute_names = ['Sepal_Length (cm)','Sepal_Width (cm)','Petal_Length (cm)','Petal_Width (cm)','Class']
-#with read_csv() we are reading the Iris dataframe and storing it in the iris varibale for further use and manipulation.
-
+# Read in the Iris dataset using the pandas read_csv() method
 iris = pd.read_csv(csv_url, names = attribute_names)
 
-#Look to the statistical data of the iris Data Set 
-print(iris.describe())
+# Display the statistical summary of the dataset using the describe() method 
+print(f"Statistical summary of the dataset: \n{iris.describe()}")
 
 # Using the value_counts() method on the 'Class' column to show how many different class Iris flower can be found in the Data Set.
+print("The Iris plant are: \n")
 print(iris["Class"].value_counts())
 
+# Display the number of dimensions in the dataset using the ndim attribute.
+print(f"The Iris Data Set has {iris.ndim} dimensions")
 
+# Display the number of rows and columns in the dataset using the shape attribute
+print(f"The Iris data set has {iris.shape[0]} rows and {iris.shape[1]} columns")
 
+# Check for any missing values in the dataset using the isnull() and sum() methods
+print(f"the number of missing value in the Data set is: \n{iris.isnull().sum()}")
 
-
+# Display information about the dataset using the info() method
+print(f"Information about the Iris Data Set:")
+print(iris.info())
 
 
 
