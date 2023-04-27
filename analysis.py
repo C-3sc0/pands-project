89315fe9b64f00cb81d677f1238de7a3855ad2d0
Francesco <G00425679@atu.ie>
2023-04-27 14:21:31 +0100

    2x2 grid histogram of the 4 attributes

--- analysis.py
+++ analysis.py
@@ -7,12 +7,16 @@ import sys
 
 
 
-# read in the dataset from the UCI Machine Learning Repository link
+#Read in the dataset from the UCI Machine Learning Repository link
 csv_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-# Define the names of the columns in the dataset
+#Define the names of the columns in the dataset
 attribute_names = ['Sepal_Length (cm)','Sepal_Width (cm)','Petal_Length (cm)','Petal_Width (cm)','Class']
-# Read in the Iris dataset using the pandas read_csv() method
+#Read in the Iris dataset using the pandas read_csv() method
 iris = pd.read_csv(csv_url, names = attribute_names)
+#Create 3 new DataFrame for the Iris Class Attribute for further analysis
+setosa = iris[iris.Class == "Iris-setosa"]
+versicolor = iris[iris.Class == "Iris-versicolor"]
+virginica = iris[iris.Class == "Iris-virginica"]
 
 #directing print output to a txt file:https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
 sys.stdout = open ("Analysis-summary.txt", "w")
@@ -30,63 +34,59 @@ print(f"The Iris Data Set has {iris.ndim} dimensions\n")
 print(f"The Iris data set has {iris.shape[0]} rows and {iris.shape[1]} columns\n")
 print(f"The number of missing value in the Data set is: \n {iris.isnull().sum()}\n")
 print(f"The statistical summary of the dataset is: \n\n{iris.describe()}\n")
+print(f"\nThe summary statistic for Iris Setosa class are:\n {setosa.describe()}")
+print(f"\nThe summary statistic for Iris Versicolor class are:\n {versicolor.describe()}")
+print(f"\nThe summary statistic for Iris Virginica class are:\n {virginica.describe()}\n")
 sys.stdout.close()
 
 #visualizing the class distribution of the iris data set into a countplot
 sns.countplot(data=iris, x="Class", saturation= 0.5)
 plt.title("Class Count")
-plt.show()
+#plt.show()
 
 #creating a histogram for all the Iris Data set attributes
 iris.hist(alpha=0.5, bins=25, figsize=(10,6))
 plt.suptitle("Histograms of Iris Dataset Features")
 #plt.savefig("Iris-data-set-histogram")
-plt.show()
+#plt.show()
 
-#create a distplot for all the 4 attributes:
-setosa = iris[iris.Class == "Iris-setosa"]
-versicolor = iris[iris.Class == "Iris-versicolor"]
-virginica = iris[iris.Class == "Iris-virginica"]
+#create a 2x2 histplot to compare the 4 attributes:
+fig, ax = plt.subplots(2,2, figsize=(10,6))
+sns.histplot(data=iris, x="Sepal_Length (cm)", hue="Class", kde=False, linewidth=0, bins=25, element="step", ax= ax[0,0])
+sns.histplot(data=iris, x="Sepal_Width (cm)", hue="Class", kde=False, linewidth=0,bins=25, element="step", ax= ax[0,1])
+sns.histplot(data=iris, x="Petal_Length (cm)", hue="Class", kde=False, linewidth=0,bins=25, element="step", ax= ax[1,0])
+sns.histplot(data=iris, x="Petal_Width (cm)", hue="Class", kde=False, linewidth=0, bins=25, element="step", ax= ax[1,1])
+fig.suptitle("Comparision of Sepal & Petal attributes between the 3 Iris flowers", size=20)
+plt.savefig("histograms-for-comparison-of-the-4-attribute.png")
+#plt.show()
+
+#Create a Scatter plot for for Sepal and Petal Attribute
+plt.figure(figsize=(8,6))
+sns.scatterplot(y=iris["Sepal_Width (cm)"], x=iris["Sepal_Length (cm)"], hue= iris["Class"], s=50)
+plt.title("Comparison of Sepal Sizes Across Iris Species")
+#plt.savefig("Iris-Sepal-attribute-scatterplot.png")
+#plt.show()
 
 plt.figure(figsize=(8,6))
-sns.distplot(setosa["Sepal_Length (cm)"],  kde = False, label = "Iris setosa")
-sns.distplot(versicolor["Sepal_Length (cm)"],  kde = False, label = "Iris versicolor")
-sns.distplot(virginica["Sepal_Length (cm)"],  kde = False, label = "Iris virginica")
-plt.title("Sepal length in cm", size = 12)
-plt.ylabel("Frequency", size = 9)
-plt.legend()
-#plt.savefig("Sepal-Length-distplot.png")
+sns.scatterplot(y=iris["Petal_Width (cm)"], x=iris["Petal_Length (cm)"], hue= iris["Class"], s=50)
+plt.title("Comparison of Petal Sizes Across Iris Species")
+#plt.savefig("Iris-Petal-attribute-scatterplot.png")
+#plt.show()
+
+fig, ax = plt.subplots(2,2, figsize=(16,9))
+font = {"family" : "Lucida Handwriting", "color" : "Blue", "size" : 15}
+sns.violinplot( x= iris["Class"], y= iris["Sepal_Length (cm)"], data= iris,  ax= ax[0,0])
+sns.violinplot( x= iris["Class"], y= iris["Sepal_Width (cm)"], data= iris, ax= ax[0,1])
+sns.violinplot( x= iris["Class"], y= iris["Petal_Length (cm)"], data= iris,  ax= ax[1,0])
+sns.violinplot( x= iris["Class"], y= iris["Petal_Width (cm)"], data= iris,  ax= ax[1,1])
+fig.suptitle("Violinplot of Iris Sepal & Petal attributes", size=20, fontdict=font)
+plt.savefig("Violinlot-of-Iris-Data-Set.png")
+#plt.show()
+
+sns.pairplot(iris, hue= "Class")
+plt.savefig("Iris-data-set-pairplot.png")
 plt.show()
 
-plt.figure(figsize=(8,6))
-sns.distplot(setosa["Sepal_Width (cm)"],  kde = False, label = "Iris setosa")
-sns.distplot(versicolor["Sepal_Width (cm)"],  kde = False, label = "Iris versicolor")
-sns.distplot(virginica["Sepal_Width (cm)"],  kde = False, label = "Iris virginica")
-plt.title("Sepal Width in cm", size = 12)
-plt.ylabel("Frequency", size = 9)
-plt.legend()
-#plt.savefig("Sepal-Width-distplot.png")
-plt.show()
-
-plt.figure(figsize=(8,6))
-sns.distplot(setosa["Petal_Length (cm)"],  kde = False, label = "Iris setosa")
-sns.distplot(versicolor["Petal_Length (cm)"], kde = False, label = "Iris versicolor")
-sns.distplot(virginica["Petal_Length (cm)"],  kde = False, label = "Iris virginica")
-plt.title("Petal length in cm", size = 12)
-plt.ylabel("Frequency", size = 9)
-plt.legend()
-#plt.savefig("Petal-Length-distplot.png")
-plt.show()
-
-plt.figure(figsize=(8,6))
-sns.distplot(setosa["Petal_Width (cm)"],  kde = False, label = "Iris setosa")
-sns.distplot(versicolor["Petal_Width (cm)"],  kde = False, label = "Iris versicolor")
-sns.distplot(virginica["Petal_Width (cm)"],  kde = False, label = "Iris virginica")
-plt.title("Petal Width in cm", size = 12)
-plt.ylabel("Frequency", size = 9)
-plt.legend()
-#plt.savefig("Petal-Width-distplot.png")
-plt.show()
 
 
 
